6626a9c603320252d65b92f634ea17ca28999d2b
Ingo Boehme <ingo@puco.de>
2020-06-10 12:27:38 +0200

    Initial commit

--- Package.swift
+++ Package.swift
@@ -5,19 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "IBGeoSelection",
+    platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "IBGeoSelection",
             targets: ["IBGeoSelection"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(path: "https://github.com/Tintenklecks/IBExtensions.git")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "IBGeoSelection",
             dependencies: []),
